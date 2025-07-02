ae5b76bd575fb763bfb2e5a79e68720caab87e78
ChamashaFernando <chamashanethmi81@gmail.com>
2025-07-02 12:19:08 +0530

    Initial commit - add spring boot project

---
+++ src/main/java/lk/chamasha/flyway/example/FlywayExampleApiApplication.java
@@ -0,0 +1,13 @@
+package lk.chamasha.flyway.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayExampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayExampleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/chamasha/flyway/example/FlywayExampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.chamasha.flyway.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayExampleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
